aee92c0bc566d0988df5c5f0d7234a832d72e1df
peynadol <peynadol@gmail.com>
2025-04-04 13:48:27 +0100

    add path for render.com

--- db/database.js
+++ db/database.js
@@ -1,4 +1,6 @@
+
 const Database = require("better-sqlite3")
+const fs = require("fs")
 const path = require("path")
 
 const renderDbPath = "/opt/render/.data/db.sqlite"
@@ -6,19 +8,24 @@ const localDbPath = path.join(__dirname, "db.sqlite")
 
 const dbPath = process.env.NODE_ENV === "production" ? renderDbPath : localDbPath
 
+if (process.env.NODE_ENV === "production") {
+	const dbFolder = path.dirname(renderDbPath)
+	if (!fs.existsSync(dbFolder)) {
+		fs.mkdirSync(dbFolder, { recursive: true })
+	}
+}
+
 const db = new Database(dbPath)
 
-// create table
+// Create table if it doesn't already exist
 db.exec(`
 CREATE TABLE IF NOT EXISTS destinations(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-location TEXT NOT NULL,
-country TEXT NOT NULL,
-notes TEXT,
-imageUrl TEXT
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  location TEXT NOT NULL,
+  country TEXT NOT NULL,
+  notes TEXT,
+  imageUrl TEXT
 )
 `)
-
-
 
 module.exports = db
